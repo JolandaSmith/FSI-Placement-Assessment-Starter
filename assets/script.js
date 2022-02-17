7e33a5fd29a0fcf07486e26bc951fc4c97c0f0d9
cameron smith <cam@camerons-MBP.fios-router.home>
2022-02-16 21:34:05 -0500

    update

--- assets/script.js
+++ assets/script.js
@@ -9,9 +9,8 @@ const gbMinusBtn = document.querySelector("#minus-gb")
 // TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
 
 gbPlusBtn.addEventListener("click", function () {
-  gb++
-let UpdateQuantity = +1
-UpdateQuantity = +1 
+ gb++
+
 }); 
 console.log('Gingerbread + gbPlusBtn') 
 
@@ -19,11 +18,46 @@ gbMinusBtn.addEventListener("click", function (e) {
   if (gb > 0) {
     gb--
   }
-  let UpdateQuantity = -1
-  UpdateQuantity = -1  
+  
 });
 console.log('Gingerbread - gbMinusBtn') 
 
+const ccPlusBtn = document.querySelector("#add-cc")
+const ccMinusBtn = document.querySelector("#minus-cc")
+
+ccPlusBtn.addEventListener("click", function () {
+    cc++
+   
+   }); 
+   console.log('ChoclateChip + ccPlusBtn') 
+
+   ccMinusBtn.addEventListener("click", function (e) {
+    if (cc > 0) {
+      cc--
+    }
+    
+  });
+  console.log('ChoclateChip - ccMinusBtn') 
+
+const sugarPlusBtn = document.querySelector("#add-sugar")
+const sugarMinusBtn = document.querySelector("#minus-sugar")
+
+sugarPlusBtn.addEventListener("click", function () {
+    sugar++
+   
+   }); 
+   console.log('SugarSprinkle + sugarPlusBtn') 
+
+   ccMinusBtn.addEventListener("click", function (e) {
+    if (sugar > 0) {
+      sugar--
+    }
+    
+  });
+  console.log('SugarSprinkle - sugarMinusBtn') 
+
+
+   
 let yourName = "Jolanda Smith"
 console.log(yourName) 
 const credit = document.querySelector('#credit') 
